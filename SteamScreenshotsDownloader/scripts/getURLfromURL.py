2fa940f4640e9d2dff0b1ec8fb072eaa20efc175
OsakaBlue <38051097+OsakaBlue@users.noreply.github.com>
2024-04-02 09:23:12 +0300

    1.2
    Added processed link counter
    Fixed link list not creating due to forbidden symbols in game name (maybe?)

--- SteamScreenshotsDownloader/scripts/getURLfromURL.py
+++ SteamScreenshotsDownloader/scripts/getURLfromURL.py
@@ -11,6 +11,12 @@ def save_screenshot_url(screenshot_url, game_name):
     with open(filename, "a") as file:
         file.write(screenshot_url + "\n")
 
+# Підрахунок загальної кількості рядків у файлі urls.txt
+total_lines = sum(1 for line in open("scripts/temp/urls.txt"))
+
+# Лічильник оброблених посилань
+processed_links = 0
+
 # Зчитуємо посилання з файлу urls.txt та виконуємо код для кожного з них
 with open("scripts/temp/urls.txt", "r") as file:
     for line in file:
@@ -23,27 +29,40 @@ with open("scripts/temp/urls.txt", "r") as file:
         # Парсинг HTML-коду
         soup = BeautifulSoup(html_content, "html.parser")
 
-        # Перевірка наявності елемента з назвою гри
-        app_name_element = soup.find("div", class_="apphub_AppName ellipsis")
-        if app_name_element:
-            game_name = app_name_element.text.strip()
-            game_name = game_name.replace(":", "")  # Усуваємо символ ":" у назві гри для назви файлу
-        else:
-            # Якщо назву гри не знайдено, шукаємо посилання з атрибутом data-panel
-            game_name_element = soup.find("a", {"data-panel": True})
-            if game_name_element:
-                game_name = game_name_element.text.strip()
+        try:
+            # Перевірка наявності елемента з назвою гри
+            app_name_element = soup.find("div", class_="apphub_AppName ellipsis")
+            if app_name_element:
+                game_name = app_name_element.text.strip()
+                game_name = game_name.replace(":", "").replace("/", "-")  # Усуваємо символи ":" та "/" у назві гри для назви файлу
             else:
-                print(f"Could not find game's name on {url} page... Skipping")
-                continue
+                # Якщо назву гри не знайдено, шукаємо посилання з атрибутом data-panel
+                game_name_element = soup.find("a", {"data-panel": True})
+                if game_name_element:
+                    game_name = game_name_element.text.strip()
+                else:
+                    print(f"Could not find game's name on {url} page... Skipping")
+                    continue
 
-        actual_media = soup.find("div", class_="actualmediactn")
-        if actual_media:
-            screenshot_url = actual_media.find("a")["href"]
-            save_screenshot_url(screenshot_url, game_name)
-            print(f"Screenshot link from {url} page saved in {game_name}.txt")
-        else:
-            print(f"Could not find screenshot link on {url} page... Skipping")
+            actual_media = soup.find("div", class_="actualmediactn")
+            if actual_media:
+                screenshot_url = actual_media.find("a")["href"]
+                save_screenshot_url(screenshot_url, game_name)
+                processed_links += 1
+                print(f"Screenshot link from {url} page saved in {game_name}.txt")
+
+                # Вивід поточної кількості оброблених посилань кожні 10 посилань
+                if processed_links % 10 == 0:
+                    print(f"{processed_links}/{total_lines} links processed.")
+
+            else:
+                print(f"Could not find screenshot link on {url} page... Skipping")
+        except Exception as e:
+            print(f"An error occurred while processing {url}: {str(e)}")
+            continue
+
+# Вивід кінцевого результату
+print(f"{processed_links}/{total_lines} links processed.")
 
 print("Screenshots link list created successfully. Proceeding...")
 print("\n")
